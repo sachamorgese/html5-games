cdc1cf5355c6c334df89ff12c64fbf44c2cf0746
Sacha Morgese <sachamorgese@gmail.com>
2018-08-23 18:05:25 +0900

    Deleted the Boilerplate to leave the shooting game

--- Game Library Boilerplate/src/main.js
+++
@@ -1,9 +0,0 @@
-import pop from '../raptor/';
-const { Game, Sprite, Texture } = pop;
-const game = new Game(640, 320);
-const { scene, w, h } = game;
-
-// Game objects
-
-// Our old loop!
-game.run((dt, t) => {});
